049a15d5c99e4ea0eb0425d04c6e2367570ab387
jiwon2724 <stopone3119@naver.com>
2023-09-25 05:47:50 +0900

    [UI] 기본 화면 BottomNavigation 추가 및 프래그먼트 연동

--- feature/poketmon/src/main/java/com/poketmonencyclopedia/poketmon/PoketmonActivity.kt
+++ feature/poketmon/src/main/java/com/poketmonencyclopedia/poketmon/PoketmonActivity.kt
@@ -1,11 +1,29 @@
 package com.poketmonencyclopedia.poketmon
 
+import androidx.fragment.app.Fragment
+import com.poketmonencyclopedia.feature.poketmon.R
 import com.poketmonencyclopedia.feature.poketmon.databinding.ActivityPoketmonBinding
+import com.poketmonencyclopedia.poketmondetail.PoketmonDetailFragment
+import com.poketmonencyclopedia.poketmonlist.PoketmonListFragment
 import com.poketmonencyclopedia.ui.BaseActivity
+import com.poketmonencyclopedia.ui.R as UI
 
 class PoketmonActivity : BaseActivity<ActivityPoketmonBinding>() {
     override fun bindingFactory() = ActivityPoketmonBinding.inflate(layoutInflater)
     override fun initView() {
+        // TODO : 데이터 매핑 후 개선
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                UI.id.detail -> replaceFragment(PoketmonDetailFragment())
+                else -> replaceFragment(PoketmonListFragment())
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
 
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.bottomNavigationView, fragment)
+        fragmentTransaction.commit()
     }
 }
