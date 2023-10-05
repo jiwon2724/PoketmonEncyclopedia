74b97e308f7689d17627cf29beaf63e93628771c
정지원 <stopone3119@naver.com>
2023-10-05 16:27:45 +0900

    [FEATURE] (#22)
    api 비동기 처리 및 하나의 Flow로 튜닝
    api response 데이터 매핑 (포켓몬 이미지, 기술, 설명, 등)

--- core/data/src/main/java/com/poketmonencyclopedia/detail/PoketmonListRepositoryImpl.kt
+++ core/data/src/main/java/com/poketmonencyclopedia/detail/PoketmonListRepositoryImpl.kt
@@ -1,25 +1,40 @@
 package com.poketmonencyclopedia.detail
 
+import com.poketmonencyclopedia.detail.model.ability.AbilityResponse
+import com.poketmonencyclopedia.domain.detail.DetailResponse
+import com.poketmonencyclopedia.detail.model.information.InformationResponse
+import com.poketmonencyclopedia.detail.model.species.SpeciesResponse
 import com.poketmonencyclopedia.domain.detail.PoketmonDetailRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.zip
 import javax.inject.Inject
 
-class PoketmonListRepositoryImpl @Inject constructor(
-    poketmonDetailApi: PoketmonDetailApi
-) : PoketmonDetailRepository {
-    override suspend fun getPoketmonInformation(): Flow<Int> {
-        TODO("Not yet implemented")
+class PoketmonListRepositoryImpl @Inject constructor(private val poketmonDetailApi: PoketmonDetailApi) : PoketmonDetailRepository {
+    private suspend fun getPoketmonInformation(): Flow<InformationResponse> = flow {
+        poketmonDetailApi.getPoketmonInformation()
     }
 
-    override suspend fun getPoketmonAbility(): Flow<Int> {
-        TODO("Not yet implemented")
+    private suspend fun getPoketmonAbility(): Flow<AbilityResponse> = flow {
+        poketmonDetailApi.getPoketmonAbility()
     }
 
-    override suspend fun getPoketmonSpecies(): Flow<Int> {
-        TODO("Not yet implemented")
+    private suspend fun getPoketmonSpecies(): Flow<SpeciesResponse> = flow {
+        poketmonDetailApi.getPoketmonSpecies()
     }
 
-    override suspend fun getPoketmonDetail(): Flow<Int> {
-        TODO("Not yet implemented")
+    override suspend fun getPoketmonDetail(): Flow<DetailResponse> {
+        val detailResponse = DetailResponse()
+        val mergeResult = getPoketmonAbility().zip(getPoketmonSpecies()) { abilityResponse, speciesResponse ->
+            detailResponse.copy(
+                name = speciesResponse.name,
+                description = speciesResponse.form_descriptions.toString(),
+                type = speciesResponse.gender_rate.toString(),
+                ability = abilityResponse.name
+            )
+        }.zip(getPoketmonInformation()) { _, informationResponse ->
+            detailResponse.copy(information = informationResponse.name)
+        }
+        return mergeResult
     }
 }
--- core/domain/src/main/java/com/poketmonencyclopedia/domain/detail/PoketmonDetailRepository.kt
+++ core/domain/src/main/java/com/poketmonencyclopedia/domain/detail/PoketmonDetailRepository.kt
@@ -4,8 +4,5 @@ import kotlinx.coroutines.flow.Flow
 
 interface PoketmonDetailRepository {
     // TODO : getPoketmonDetail ResponseType 설계, Mapper구현
-    suspend fun getPoketmonInformation(): Flow<Int>
-    suspend fun getPoketmonAbility(): Flow<Int>
-    suspend fun getPoketmonSpecies(): Flow<Int>
-    suspend fun getPoketmonDetail(): Flow<Int>
+    suspend fun getPoketmonDetail(): Flow<DetailResponse>
 }
